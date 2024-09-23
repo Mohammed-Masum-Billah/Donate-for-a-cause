aa12122c2dd6c8cc6c0ed00bd39db2021140c890
Mohammed-Masum-Billah <masumfahim41@gamil.com>
2024-09-24 01:37:29 +0600

    first commit

---
+++ JS/design.js
@@ -0,0 +1,15 @@
+document.getElementById('btn-noakhali').addEventListener('click',function(event){
+    event.preventDefault();
+    console.log('clicked');
+    // const accountBalance=getElementById('noa-khali').value;
+    const floodNoakhali=getElementById('flood-noakhali').value;
+    console.log(NoaKhali);
+
+    // if (NoaKhali==='100') {
+    //     console.log('Thanks for contribution');
+        
+    // } else {
+    //     console.log('Try again');
+        
+    // }
+})
